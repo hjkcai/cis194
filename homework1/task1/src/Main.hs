bcf97cab9f24a8934b943580b3745c4daa666a92
Jingkun Hua <huajingkun1214@gmail.com>
2019-11-16 00:55:21 +0800

    h1t1: impl toDigit and toDigitRev

--- homework1/task1/src/Main.hs
+++ homework1/task1/src/Main.hs
@@ -1,5 +1,13 @@
 module Main where
 
+toDigitsRev :: Integer -> [Integer]
+toDigitsRev num
+  | num <= 0  = []
+  | otherwise = (num `mod` 10) : toDigitsRev(num `div` 10)
+
+toDigits :: Integer -> [Integer]
+toDigits = reverse . toDigitsRev
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  putStrLn $ show $ toDigitsRev 1234567890
