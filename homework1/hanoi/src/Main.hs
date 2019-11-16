7becec4d0146238cce041168470e110dc2b9519d
Jingkun Hua <huajingkun1214@gmail.com>
2019-11-17 00:36:11 +0800

    h1-hanoi: solve

--- homework1/hanoi/src/Main.hs
+++ homework1/hanoi/src/Main.hs
@@ -1,5 +1,19 @@
 module Main where
 
+type Peg = String
+type Move = (Peg, Peg)
+
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi discs from to temp
+  | discs == 1 = [(from, to)]
+  | discs == 2 = [(from, temp), (from, to), (temp, to)]
+  | otherwise  = hanoi (discs - 1) from temp to ++ [(from, to)] ++ hanoi (discs - 1) temp to from
+
+hanoiA :: Integer -> [Move]
+hanoiA discs = hanoi discs "a" "b" "c"
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  print $ hanoiA 2
+  print $ hanoiA 3
+  print $ hanoiA 4
