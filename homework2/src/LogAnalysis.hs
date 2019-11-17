8af9e96534871924ee9e0cf51a407226df1f7f53
hjkcai <huajingkun1214@gmail.com>
2019-11-18 00:06:45 +0800

    hw2: impl parseMessage

--- homework2/src/LogAnalysis.hs
+++ homework2/src/LogAnalysis.hs
@@ -3,6 +3,13 @@ module LogAnalysis where
 
 import Log
 
-main :: IO ()
-main = do
-  putStrLn "hello world"
+-- Exercise 1
+parseMessage :: String -> LogMessage
+parseMessage msg = parseMessageWords $ words msg
+  where parseMessageWords ("I":timestamp:xs) = LogMessage Info (read timestamp) (unwords xs)
+        parseMessageWords ("W":timestamp:xs) = LogMessage Info (read timestamp) (unwords xs)
+        parseMessageWords ("E":code:timestamp:xs) = LogMessage (Error (read code)) (read timestamp) (unwords xs)
+        parseMessageWords xs = Unknown $ unwords xs
+
+parse :: String -> [LogMessage]
+parse = map parseMessage . lines
