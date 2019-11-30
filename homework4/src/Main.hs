02f8b746c6a22ec8e5216424db7af1466de569b2
hjkcai <huajingkun1214@gmail.com>
2019-11-30 15:16:57 +0800

    hw4: complete exercise 1

--- homework4/src/Main.hs
+++ homework4/src/Main.hs
@@ -1,1 +1,14 @@
 module Main where
+
+-- Exercise 1
+fun1 :: [Integer] -> Integer
+fun1 [] = 1
+fun1 (x:xs)
+  | even x = (x - 2) * fun1 xs
+  | otherwise = fun1 xs
+
+productMinus2Even :: [Integer] -> Integer
+productMinus2Even = product . map (subtract 2) . filter even
+
+main :: IO ()
+main = return ()
