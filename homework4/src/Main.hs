e3ead3fefccb19fc5f1423e9a5c15e4e93ea17a9
hjkcai <huajingkun1214@gmail.com>
2019-11-30 21:16:40 +0800

    hw4: complete exercise 1.2

--- homework4/src/Main.hs
+++ homework4/src/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
--- Exercise 1
+-- Exercise 1.1
 fun1 :: [Integer] -> Integer
 fun1 [] = 1
 fun1 (x:xs)
@@ -9,6 +9,21 @@ fun1 (x:xs)
 
 productMinus2Even :: [Integer] -> Integer
 productMinus2Even = product . map (subtract 2) . filter even
+
+-- Exercise 1.2
+-- fun2 10 = 10 + fun2 5 = 10 + fun2 16 = 10 + (16 + fun2 8) = 10 + (16 + (8 + fun2 4)) = ...
+fun2 :: Integer -> Integer
+fun2 1 = 0
+fun2 n
+  | even n = n + fun2 (n `div` 2)
+  | otherwise = fun2 (3 * n + 1)
+
+specialSum :: Integer -> Integer
+specialSum = sum . filter even . takeWhile (/=0) . iterate it
+  where it n
+          | n == 1    = 0
+          | even n    = n `div` 2
+          | otherwise = 3 * n + 1
 
 main :: IO ()
 main = return ()
